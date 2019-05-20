b81257e47c047e1e150bc17e20b2871667ab9593
Eymric <gatibelza.eymric@gmail.com git config --global user.name Eymric>
2019-05-20 16:18:33 +0200

    init eymric

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,16 +78,16 @@
             @endif
 
             <div class="content">
+                        <img src="{{ asset('img/rhhh.jpg') }}">
+
                 <div class="title m-b-md">
-                    Laravel
+                    Gestion RH
                 </div>
 
                 <div class="links">
-                    <a href="{{ url('/test') }}">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('salaries/create') }}">Salariés</a>
+                    <a href="https://laracasts.com">Absence</a>
+                    <a href="https://laravel-news.com">Candidats</a>
                 </div>
             </div>
         </div>
